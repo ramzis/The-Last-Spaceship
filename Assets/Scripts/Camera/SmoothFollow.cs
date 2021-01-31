01fe867297000f328f83b721d21f497976900ba6
ramzis <ramzis@github.com>
2021-01-31 12:26:21 +0000

    Fix textboxmanager, better zooming camera

--- Assets/Scripts/Camera/SmoothFollow.cs
+++ Assets/Scripts/Camera/SmoothFollow.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using System.Collections;
 
@@ -7,8 +8,23 @@ public class SmoothFollow : MonoBehaviour
     public float smoothTime = 0.3F;
     private Vector3 velocity = Vector3.zero;
 
+    private Camera cam;
+    private Rigidbody2D targetRb;
+
+    private void Start()
+    {
+        targetRb = target.GetComponent<Rigidbody2D>();
+        cam = GetComponent<Camera>();
+    }
+
     void Update()
     {
+        if (targetRb != null && cam != null)
+        {
+            var targetSize = Mathf.Clamp(targetRb.velocity.magnitude * 2f, 4f, 14f);
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetSize, Time.deltaTime);
+        }
+
         // Define a target position above and behind the target transform
         Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, -10));
 
